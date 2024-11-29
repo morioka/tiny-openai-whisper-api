cc6d20460cebea879be06f3f42326fbe8f598f78
Yasuhiro Morioka <ymorioka@gmail.com>
2024-11-29 23:13:00 +0900

    fix test.sh

--- tests/openai/test_audio_transcription.sh
+++ tests/openai/test_audio_transcription.sh
@@ -1,2 +1,5 @@
-wget https://openaiassets.blob.core.windows.net/\$web/API/docs/audio/alloy.wav
+#!/bin/bash
+if [ ! -e alloy.wav ]; then
+  wget https://openaiassets.blob.core.windows.net/\$web/API/docs/audio/alloy.wav
+fi
 curl http://localhost:8000/v1/audio/transcriptions   -H "Content-Type: multipart/form-data"   -F model="whisper-1"   -F file="@alloy.wav"   -F response_format=text
